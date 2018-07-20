53376742bdd4bc24f2623eaa10d42df3fd2b4a80
Mona Lisa <mona@lisa.com>
2018-07-20 04:26:49 +0300

    Rewrite and write new tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,12 +3,23 @@ import genDiff from '../src';
 
 const path = '__tests__/__fixtures__/';
 
-test('json', () => {
-  const expected1 = fs.readFileSync(`${path}1/expected`).toString();
-  expect(genDiff(`${path}1/before.json`, `${path}1/after.json`)).toBe(expected1);
+test('1', () => {
+  const e1 = fs.readFileSync(`${path}1/e`).toString();
+  expect(genDiff(`${path}1/b.json`, `${path}1/a.json`)).toBe(e1);
 });
-
-test('yml', () => {
-  const expected2 = fs.readFileSync(`${path}2/expected`).toString();
-  expect(genDiff(`${path}2/before.yml`, `${path}2/after.yml`)).toBe(expected2);
+test('2', () => {
+  const e2 = fs.readFileSync(`${path}2/e`).toString();
+  expect(genDiff(`${path}2/b.json`, `${path}2/a.json`)).toBe(e2);
+});
+test('3', () => {
+  const e3 = fs.readFileSync(`${path}3/e`).toString();
+  expect(genDiff(`${path}3/b.json`, `${path}3/a.json`)).toBe(e3);
+});
+test('4', () => {
+  const e4 = fs.readFileSync(`${path}4/e`).toString();
+  expect(genDiff(`${path}4/b.json`, `${path}4/a.json`)).toBe(e4);
+});
+test('5', () => {
+  const e5 = fs.readFileSync(`${path}5/e`).toString();
+  expect(genDiff(`${path}5/b.json`, `${path}5/a.json`)).toBe(e5);
 });
